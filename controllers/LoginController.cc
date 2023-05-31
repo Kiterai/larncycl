28a50ceebd73d86f38ec2b27cc6b716f299bd91f
kiterai <kiterai@foolslab.net>
2023-05-31 09:18:46 +0900

    hash/verify

--- controllers/LoginController.cc
+++ controllers/LoginController.cc
@@ -1,5 +1,43 @@
 #include "LoginController.h"
-#include <argon2.h>
+#include <argon2.h> 
+#include <random>
+
+namespace Passwd {
+
+auto hash(std::string passwd) {
+    constexpr size_t itCost = 1;
+    constexpr size_t memCost = 64;
+    constexpr size_t parallel = 2;
+    constexpr size_t bufSz = 128;
+    constexpr size_t saltSz = 16;
+    constexpr size_t hashSz = 32;
+
+    char buf[bufSz];
+    char salt[saltSz];
+
+    std::random_device rndgen;
+    for (size_t i = 0; i < saltSz; i++) {
+        salt[i] = rndgen() - rndgen.min();
+    }
+
+    int res = argon2id_hash_encoded(itCost, memCost, parallel, passwd.c_str(), passwd.length(), salt, saltSz, hashSz, buf, bufSz);
+    if (res != ARGON2_OK) {
+        throw std::runtime_error(std::string("error occured on argon2id_hash_encoded(): ") + std::to_string(res));
+    }
+
+    return std::string(buf);
+}
+
+bool verify(std::string passwd, std::string hash) {
+    auto res = argon2id_verify(hash.c_str(), passwd.c_str(), passwd.length());
+    if (res == ARGON2_OK)
+        return true;
+    if (res == ARGON2_VERIFY_MISMATCH)
+        return false;
+    throw std::runtime_error(std::string("error occured on argon2id_verify(): ") + std::to_string(res));
+}
+
+}; // namespace Passwd
 
 // Add definition of your processing function here
 void LoginController::login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) const {
